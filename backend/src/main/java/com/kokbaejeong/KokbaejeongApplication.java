e24c070f513450ebd7ac66ae8805d77d63aa61fc
mingyukang5420 <mingyukang@kakao.com>
2026-09-08 17:05:13 +0900

    chore: set up spring boot backend skeleton for local development

---
+++ backend/src/main/java/com/kokbaejeong/KokbaejeongApplication.java
@@ -0,0 +1,13 @@
+package com.kokbaejeong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KokbaejeongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KokbaejeongApplication.class, args);
+	}
+
+}
