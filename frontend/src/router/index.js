bcdcc61e16058f81eec9d5131dffd2fd04c6a971
mingyukang5420 <mingyukang@kakao.com>
2026-09-09 11:08:46 +0900

    feat: implement admin warehouse management screens

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import AssignView from '../views/AssignView.vue'
 import CancelView from '../views/CancelView.vue'
 import AdminLoginView from '../views/admin/AdminLoginView.vue'
+import AdminWarehouseListView from '../views/admin/AdminWarehouseListView.vue'
+import AdminWarehouseFormView from '../views/admin/AdminWarehouseFormView.vue'
+import { isLoggedIn } from '../api/adminClient'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +12,31 @@ const router = createRouter({
     { path: '/w/:warehouseId', name: 'assign', component: AssignView },
     { path: '/assignments/:id/cancel', name: 'cancel', component: CancelView },
     { path: '/admin', name: 'admin-login', component: AdminLoginView },
+    {
+      path: '/admin/warehouses',
+      name: 'admin-warehouses',
+      component: AdminWarehouseListView,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/admin/warehouses/new',
+      name: 'admin-warehouse-new',
+      component: AdminWarehouseFormView,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/admin/warehouses/:id/edit',
+      name: 'admin-warehouse-edit',
+      component: AdminWarehouseFormView,
+      meta: { requiresAuth: true },
+    },
   ],
+})
+
+router.beforeEach((to) => {
+  if (to.meta.requiresAuth && !isLoggedIn()) {
+    return { name: 'admin-login' }
+  }
 })
 
 export default router
